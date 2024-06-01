e097a574d7da17e994203064f1c67131018f51a7
KyKirma <pedrohenriquekourly@hotmail.com>
2024-06-01 01:17:54 -0300

    Update Theme

--- routes.py
+++ routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, redirect, url_for
-from main import app, db
-from models import Campeonato, TPC
+from main import app
+from models import Campeonato, TPC, Jogo
 
 @app.route('/')
 def home():
@@ -19,4 +19,24 @@ def campEscolhido(nome):
     timesNoCamp = TPC.query.filter_by(CampAno = str(info[2]), Divisao = str(info[0])).all()
 
     return render_template('times.html',
-                           lista = timesNoCamp)
+                           lista = timesNoCamp,
+                           nome = nome)
+
+@app.route('/previsoes/<nome>/<time>')
+def timeEscolhido(nome, time):
+    #O nome vem no formato: 'Div Regiao Ano'
+    jogosTime1 = Jogo.query.filter_by(nome = nome, hometeam = time)
+    jogosTime2 = Jogo.query.filter_by(nome = nome, awayteam = time)
+
+    info = nome.split()
+    timesNoCamp = TPC.query.filter_by(CampAno = str(info[2]), Divisao = str(info[0])).all()
+
+    return render_template('times.html',
+                           listaJ1 = jogosTime1,
+                           listaJ2 = jogosTime2,
+                           lista = timesNoCamp,
+                           nome = nome)
+
+@app.route('/theme')
+def tema():
+    pass
