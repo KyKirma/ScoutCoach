c28b78152ac6fb6cff18c769dd8c9182da2527d6
KyKirma <pedrohenriquekourly@hotmail.com>
2024-06-25 15:31:14 -0300

    Update percorrer campeonatos

--- webscrapper.py
+++ webscrapper.py
@@ -24,7 +24,7 @@ import time
 import pandas as pd
 from tqdm import tqdm
 
-driver.get("https://www.flashscore.com.br/futebol/brasil/brasileirao-betano/resultados/") 
+driver.get("https://www.flashscore.com.br/futebol/brasil/brasileirao-betano/arquivo/") 
 time.sleep(2)
 
 # Dados coletados
@@ -72,160 +72,173 @@ dados = {
     "AATKP": []  # Ataques Perigosos
 }
 
-# Pega os jogos do ano
-eventos = driver.find_elements(By.CSS_SELECTOR, 'div.event__match--twoLine')
-links = []
-for evento in eventos:
+# Algoritmo para cada campeonato
+eventosCampeonato = driver.find_elements(By.CSS_SELECTOR, 'div.archive__row')
+linksCamp = []
+for evento in eventosCampeonato:
     link = evento.find_element(By.CSS_SELECTOR, 'a')
-    links.append(link.get_attribute("href"))
+    linksCamp.append(f"{link.get_attribute("href")}/resultados")
 
-for link in tqdm(links, total = len(links)):
-    try:
-        driver.get(link)
-        time.sleep(1)
+# Filtra apenas para os ultimos 10 cameponatos
+linksCamp = linksCamp[:2]
 
-        # Pega os principais atributos
-        nomes = driver.find_elements(By.CSS_SELECTOR, 'a.participant__participantName')
-        nomeHome = nomes[0].text
-        nomeAway = nomes[1].text
-        hora = driver.find_element(By.CSS_SELECTOR, 'div.duelParticipant__startTime')
+for campeonato in tqdm(linksCamp, total = len(linksCamp)):
+    driver.get(campeonato)
+    time.sleep(1)
+    # Pega os jogos do ano
+    eventos = driver.find_elements(By.CSS_SELECTOR, 'div.event__match--twoLine')
+    links = []
+    for evento in eventos:
+        link = evento.find_element(By.CSS_SELECTOR, 'a')
+        links.append(link.get_attribute("href"))
 
-        dados['HomeTeam'].append(nomeHome)
-        dados['AwayTeam'].append(nomeAway)
-        dados['DateTime'].append(hora.text)
+    for link in tqdm(links, total = len(links)):
+        try:
+            driver.get(link)
+            time.sleep(1)
 
-        golsSpan = driver.find_element(By.CSS_SELECTOR, 'div.detailScore__wrapper')
-        golSpan = golsSpan.find_elements(By.TAG_NAME, 'span')
-        dados['FTHG'].append(golSpan[0].text)
-        dados['FTAG'].append(golSpan[2].text)
+            # Pega os principais atributos
+            nomes = driver.find_elements(By.CSS_SELECTOR, 'a.participant__participantName')
+            nomeHome = nomes[0].text
+            nomeAway = nomes[1].text
+            hora = driver.find_element(By.CSS_SELECTOR, 'div.duelParticipant__startTime')
 
-        # Vai a seção Estatísticas
-        navJogo = driver.find_elements(By.CSS_SELECTOR, 'div._tabs_i2rza_4')
-        resultadoBotao = navJogo[1].find_elements(By.TAG_NAME, 'a')
-        resultadoBotao[1].click()
-        time.sleep(1)
+            dados['HomeTeam'].append(nomeHome)
+            dados['AwayTeam'].append(nomeAway)
+            dados['DateTime'].append(hora.text)
 
-        sections = driver.find_elements(By.CSS_SELECTOR, 'div._row_bn1w5_8')
+            golsSpan = driver.find_element(By.CSS_SELECTOR, 'div.detailScore__wrapper')
+            golSpan = golsSpan.find_elements(By.TAG_NAME, 'span')
+            dados['FTHG'].append(golSpan[0].text)
+            dados['FTAG'].append(golSpan[2].text)
 
-        # Correção caso haja section de Gols Esperados e Cartões Vermelhos
-        if sections[0].find_element(By.CSS_SELECTOR, 'div._category_hyte3_4').text != 'Gols Esperados (xG)': # Se não houver a sessão 'Gols esperados'
-            i = 0
-            dados['HGE'].append("-")
-            dados['AGE'].append("-")
+            # Vai a seção Estatísticas
+            navJogo = driver.find_elements(By.CSS_SELECTOR, 'div._tabs_i2rza_4')
+            resultadoBotao = navJogo[1].find_elements(By.TAG_NAME, 'a')
+            resultadoBotao[1].click()
+            time.sleep(1)
 
-            if sections[11].find_element(By.CSS_SELECTOR, 'div._category_hyte3_4').text == 'Cartões Vermelhos': # E a sessão 'Cartões vermelhos' existir
-                j = 1 
-                HCV = sections[11].find_element(By.CSS_SELECTOR, 'div._homeValue_lgd3g_9') # *PB = Posse de Bola
-                ACV = sections[11].find_element(By.CSS_SELECTOR, 'div._awayValue_lgd3g_13')
-                dados['HCV'].append(HCV.text)
-                dados['ACV'].append(ACV.text)
-            else:
-                dados['HCV'].append(0)
-                dados['ACV'].append(0)
-                j = 0
+            sections = driver.find_elements(By.CSS_SELECTOR, 'div._row_bn1w5_8')
 
-        else: # Se tive a sessão 'Gols esperados'
+            # Correção caso haja section de Gols Esperados e Cartões Vermelhos
+            if sections[0].find_element(By.CSS_SELECTOR, 'div._category_hyte3_4').text != 'Gols Esperados (xG)': # Se não houver a sessão 'Gols esperados'
+                i = 0
+                dados['HGE'].append("-")
+                dados['AGE'].append("-")
 
-            if sections[12].find_element(By.CSS_SELECTOR, 'div._category_hyte3_4').text == 'Cartões Vermelhos': # E a sessão 'Cartões vermelhos' existir
-                j = 1   
-                HCV = sections[12].find_element(By.CSS_SELECTOR, 'div._homeValue_lgd3g_9') # *PB = Posse de Bola
-                ACV = sections[12].find_element(By.CSS_SELECTOR, 'div._awayValue_lgd3g_13')
-                dados['HCV'].append(HCV.text)
-                dados['ACV'].append(ACV.text)
-            else:
-                dados['HCV'].append(0)
-                dados['ACV'].append(0)
-                j = 0
+                if sections[11].find_element(By.CSS_SELECTOR, 'div._category_hyte3_4').text == 'Cartões Vermelhos': # E a sessão 'Cartões vermelhos' existir
+                    j = 1 
+                    HCV = sections[11].find_element(By.CSS_SELECTOR, 'div._homeValue_lgd3g_9') # *PB = Posse de Bola
+                    ACV = sections[11].find_element(By.CSS_SELECTOR, 'div._awayValue_lgd3g_13')
+                    dados['HCV'].append(HCV.text)
+                    dados['ACV'].append(ACV.text)
+                else:
+                    dados['HCV'].append(0)
+                    dados['ACV'].append(0)
+                    j = 0
 
-            i = 1
-            HGE = sections[0].find_element(By.CSS_SELECTOR, 'div._homeValue_lgd3g_9') # *GE = Gols Esperado
-            AGE = sections[0].find_element(By.CSS_SELECTOR, 'div._awayValue_lgd3g_13')
-            dados['HGE'].append(HGE.text)
-            dados['AGE'].append(AGE.text)
-        
-        HPB = sections[0 + i].find_element(By.CSS_SELECTOR, 'div._homeValue_lgd3g_9') # *PB = Posse de Bola
-        APB = sections[0 + i].find_element(By.CSS_SELECTOR, 'div._awayValue_lgd3g_13')
-        dados['HPB'].append(HPB.text)
-        dados['APB'].append(APB.text)
+            else: # Se tive a sessão 'Gols esperados'
 
-        HTG = sections[1 + i].find_element(By.CSS_SELECTOR, 'div._homeValue_lgd3g_9') # *GT = Tentativa de Gol
-        ATG = sections[1 + i].find_element(By.CSS_SELECTOR, 'div._awayValue_lgd3g_13')
-        dados['HTG'].append(HTG.text)
-        dados['ATG'].append(ATG.text)
+                if sections[12].find_element(By.CSS_SELECTOR, 'div._category_hyte3_4').text == 'Cartões Vermelhos': # E a sessão 'Cartões vermelhos' existir
+                    j = 1   
+                    HCV = sections[12].find_element(By.CSS_SELECTOR, 'div._homeValue_lgd3g_9') # *PB = Posse de Bola
+                    ACV = sections[12].find_element(By.CSS_SELECTOR, 'div._awayValue_lgd3g_13')
+                    dados['HCV'].append(HCV.text)
+                    dados['ACV'].append(ACV.text)
+                else:
+                    dados['HCV'].append(0)
+                    dados['ACV'].append(0)
+                    j = 0
 
-        HCG = sections[2 + i].find_element(By.CSS_SELECTOR, 'div._homeValue_lgd3g_9') # *CG = Chutes no Gol
-        ACG = sections[2 + i].find_element(By.CSS_SELECTOR, 'div._awayValue_lgd3g_13')
-        dados['HCG'].append(HCG.text)
-        dados['ACG'].append(ACG.text)
+                i = 1
+                HGE = sections[0].find_element(By.CSS_SELECTOR, 'div._homeValue_lgd3g_9') # *GE = Gols Esperado
+                AGE = sections[0].find_element(By.CSS_SELECTOR, 'div._awayValue_lgd3g_13')
+                dados['HGE'].append(HGE.text)
+                dados['AGE'].append(AGE.text)
+            
+            HPB = sections[0 + i].find_element(By.CSS_SELECTOR, 'div._homeValue_lgd3g_9') # *PB = Posse de Bola
+            APB = sections[0 + i].find_element(By.CSS_SELECTOR, 'div._awayValue_lgd3g_13')
+            dados['HPB'].append(HPB.text)
+            dados['APB'].append(APB.text)
 
-        HCF = sections[3 + i].find_element(By.CSS_SELECTOR, 'div._homeValue_lgd3g_9') # *CF = Chutes para Fora
-        ACF = sections[3 + i].find_element(By.CSS_SELECTOR, 'div._awayValue_lgd3g_13')
-        dados['HCF'].append(HCF.text)
-        dados['ACF'].append(ACF.text)
+            HTG = sections[1 + i].find_element(By.CSS_SELECTOR, 'div._homeValue_lgd3g_9') # *GT = Tentativa de Gol
+            ATG = sections[1 + i].find_element(By.CSS_SELECTOR, 'div._awayValue_lgd3g_13')
+            dados['HTG'].append(HTG.text)
+            dados['ATG'].append(ATG.text)
 
-        HCB = sections[4 + i].find_element(By.CSS_SELECTOR, 'div._homeValue_lgd3g_9') # *CB = Chutes Bloqueados
-        ACB = sections[4 + i].find_element(By.CSS_SELECTOR, 'div._awayValue_lgd3g_13')
-        dados['HCB'].append(HCB.text)
-        dados['ACB'].append(ACB.text)
+            HCG = sections[2 + i].find_element(By.CSS_SELECTOR, 'div._homeValue_lgd3g_9') # *CG = Chutes no Gol
+            ACG = sections[2 + i].find_element(By.CSS_SELECTOR, 'div._awayValue_lgd3g_13')
+            dados['HCG'].append(HCG.text)
+            dados['ACG'].append(ACG.text)
 
-        HFC = sections[5 + i].find_element(By.CSS_SELECTOR, 'div._homeValue_lgd3g_9') # *FC = Faltas Cobradas
-        AFC = sections[5 + i].find_element(By.CSS_SELECTOR, 'div._awayValue_lgd3g_13')
-        dados['HFC'].append(HFC.text)
-        dados['AFC'].append(AFC.text)
+            HCF = sections[3 + i].find_element(By.CSS_SELECTOR, 'div._homeValue_lgd3g_9') # *CF = Chutes para Fora
+            ACF = sections[3 + i].find_element(By.CSS_SELECTOR, 'div._awayValue_lgd3g_13')
+            dados['HCF'].append(HCF.text)
+            dados['ACF'].append(ACF.text)
 
-        HES = sections[6 + i].find_element(By.CSS_SELECTOR, 'div._homeValue_lgd3g_9') # *ES = Escanteios
-        AES = sections[6 + i].find_element(By.CSS_SELECTOR, 'div._awayValue_lgd3g_13')
-        dados['HES'].append(HES.text)
-        dados['AES'].append(AES.text)
+            HCB = sections[4 + i].find_element(By.CSS_SELECTOR, 'div._homeValue_lgd3g_9') # *CB = Chutes Bloqueados
+            ACB = sections[4 + i].find_element(By.CSS_SELECTOR, 'div._awayValue_lgd3g_13')
+            dados['HCB'].append(HCB.text)
+            dados['ACB'].append(ACB.text)
 
-        HIP = sections[7 + i].find_element(By.CSS_SELECTOR, 'div._homeValue_lgd3g_9') # *IP = Impedimentos
-        AIP = sections[7 + i].find_element(By.CSS_SELECTOR, 'div._awayValue_lgd3g_13')
-        dados['HIP'].append(HIP.text)
-        dados['AIP'].append(AIP.text)
+            HFC = sections[5 + i].find_element(By.CSS_SELECTOR, 'div._homeValue_lgd3g_9') # *FC = Faltas Cobradas
+            AFC = sections[5 + i].find_element(By.CSS_SELECTOR, 'div._awayValue_lgd3g_13')
+            dados['HFC'].append(HFC.text)
+            dados['AFC'].append(AFC.text)
 
-        HLC = sections[8 + i].find_element(By.CSS_SELECTOR, 'div._homeValue_lgd3g_9') # *LC = Laterais Cobradas
-        ALC = sections[8 + i].find_element(By.CSS_SELECTOR, 'div._awayValue_lgd3g_13')
-        dados['HLC'].append(HLC.text)
-        dados['ALC'].append(ALC.text)
+            HES = sections[6 + i].find_element(By.CSS_SELECTOR, 'div._homeValue_lgd3g_9') # *ES = Escanteios
+            AES = sections[6 + i].find_element(By.CSS_SELECTOR, 'div._awayValue_lgd3g_13')
+            dados['HES'].append(HES.text)
+            dados['AES'].append(AES.text)
 
-        HDG = sections[9 + i].find_element(By.CSS_SELECTOR, 'div._homeValue_lgd3g_9') # *DG = Defesas do Goleiro
-        ADG = sections[9 + i].find_element(By.CSS_SELECTOR, 'div._awayValue_lgd3g_13')
-        dados['HDG'].append(HDG.text)
-        dados['ADG'].append(ADG.text)
+            HIP = sections[7 + i].find_element(By.CSS_SELECTOR, 'div._homeValue_lgd3g_9') # *IP = Impedimentos
+            AIP = sections[7 + i].find_element(By.CSS_SELECTOR, 'div._awayValue_lgd3g_13')
+            dados['HIP'].append(HIP.text)
+            dados['AIP'].append(AIP.text)
 
-        HFT = sections[10 + i].find_element(By.CSS_SELECTOR, 'div._homeValue_lgd3g_9') # *FT = Faltas
-        AFT = sections[10 + i].find_element(By.CSS_SELECTOR, 'div._awayValue_lgd3g_13')
-        dados['HFT'].append(HFT.text)
-        dados['AFT'].append(AFT.text)
+            HLC = sections[8 + i].find_element(By.CSS_SELECTOR, 'div._homeValue_lgd3g_9') # *LC = Laterais Cobradas
+            ALC = sections[8 + i].find_element(By.CSS_SELECTOR, 'div._awayValue_lgd3g_13')
+            dados['HLC'].append(HLC.text)
+            dados['ALC'].append(ALC.text)
 
-        HCA = sections[11 + i + j].find_element(By.CSS_SELECTOR, 'div._homeValue_lgd3g_9') # *CA = Cartões Amarelos
-        ACA = sections[11 + i + j].find_element(By.CSS_SELECTOR, 'div._awayValue_lgd3g_13')
-        dados['HCA'].append(HCA.text)
-        dados['ACA'].append(ACA.text)
+            HDG = sections[9 + i].find_element(By.CSS_SELECTOR, 'div._homeValue_lgd3g_9') # *DG = Defesas do Goleiro
+            ADG = sections[9 + i].find_element(By.CSS_SELECTOR, 'div._awayValue_lgd3g_13')
+            dados['HDG'].append(HDG.text)
+            dados['ADG'].append(ADG.text)
 
-        HPT = sections[12 + i + j].find_element(By.CSS_SELECTOR, 'div._homeValue_lgd3g_9') # *PT = Passes Totais
-        APT = sections[12 + i + j].find_element(By.CSS_SELECTOR, 'div._awayValue_lgd3g_13')
-        dados['HPT'].append(HPT.text)
-        dados['APT'].append(APT.text)
+            HFT = sections[10 + i].find_element(By.CSS_SELECTOR, 'div._homeValue_lgd3g_9') # *FT = Faltas
+            AFT = sections[10 + i].find_element(By.CSS_SELECTOR, 'div._awayValue_lgd3g_13')
+            dados['HFT'].append(HFT.text)
+            dados['AFT'].append(AFT.text)
 
-        HPC = sections[13 + i + j].find_element(By.CSS_SELECTOR, 'div._homeValue_lgd3g_9') # *PC = Passes Completos
-        APC = sections[13 + i + j].find_element(By.CSS_SELECTOR, 'div._awayValue_lgd3g_13')
-        dados['HPC'].append(HPC.text)
-        dados['APC'].append(APC.text)
+            HCA = sections[11 + i + j].find_element(By.CSS_SELECTOR, 'div._homeValue_lgd3g_9') # *CA = Cartões Amarelos
+            ACA = sections[11 + i + j].find_element(By.CSS_SELECTOR, 'div._awayValue_lgd3g_13')
+            dados['HCA'].append(HCA.text)
+            dados['ACA'].append(ACA.text)
 
-        HATK = sections[14 + i + j].find_element(By.CSS_SELECTOR, 'div._homeValue_lgd3g_9') # *ATK = Ataques
-        AATK = sections[14 + i + j].find_element(By.CSS_SELECTOR, 'div._awayValue_lgd3g_13')
-        dados['HATK'].append(HATK.text)
-        dados['AATK'].append(AATK.text)
+            HPT = sections[12 + i + j].find_element(By.CSS_SELECTOR, 'div._homeValue_lgd3g_9') # *PT = Passes Totais
+            APT = sections[12 + i + j].find_element(By.CSS_SELECTOR, 'div._awayValue_lgd3g_13')
+            dados['HPT'].append(HPT.text)
+            dados['APT'].append(APT.text)
 
-        HATKP = sections[15 + i + j].find_element(By.CSS_SELECTOR, 'div._homeValue_lgd3g_9') # *ATKP = Ataques Perigosos
-        AATKP = sections[15 + i + j].find_element(By.CSS_SELECTOR, 'div._awayValue_lgd3g_13')
-        dados['HATKP'].append(HATKP.text)
-        dados['AATKP'].append(AATKP.text)
+            HPC = sections[13 + i + j].find_element(By.CSS_SELECTOR, 'div._homeValue_lgd3g_9') # *PC = Passes Completos
+            APC = sections[13 + i + j].find_element(By.CSS_SELECTOR, 'div._awayValue_lgd3g_13')
+            dados['HPC'].append(HPC.text)
+            dados['APC'].append(APC.text)
 
-    except Exception as error:
-        print(f'ERRO PEDRO OKLHA AQUI: {error}')
+            HATK = sections[14 + i + j].find_element(By.CSS_SELECTOR, 'div._homeValue_lgd3g_9') # *ATK = Ataques
+            AATK = sections[14 + i + j].find_element(By.CSS_SELECTOR, 'div._awayValue_lgd3g_13')
+            dados['HATK'].append(HATK.text)
+            dados['AATK'].append(AATK.text)
+
+            HATKP = sections[15 + i + j].find_element(By.CSS_SELECTOR, 'div._homeValue_lgd3g_9') # *ATKP = Ataques Perigosos
+            AATKP = sections[15 + i + j].find_element(By.CSS_SELECTOR, 'div._awayValue_lgd3g_13')
+            dados['HATKP'].append(HATKP.text)
+            dados['AATKP'].append(AATKP.text)
+
+        except Exception as error:
+            print(f'ERRO PEDRO OKLHA AQUI: {error}')
 
 df = pd.DataFrame(dados)
-filename = 'datasetFlashScore.csv'
+filename = 'datasetFlashScore2.csv'
 df.to_csv(filename, sep = ';', index = False)
