66a7f2dae5c9bf9c48019235ab3c79140518de2d
KyKirma <pedrohenriquekourly@hotmail.com>
2024-08-12 16:55:21 -0300

    Delete na antiga versão do site, refatoração do código fonte

--- IA.py
+++ IA.py
@@ -1,16 +1,27 @@
 import pandas as pd
-
-# Lê os dois dataframes
-df1 = pd.read_csv('datasetFlashScore.csv', delimiter=';')
-df2 = pd.read_csv('datasetFlashScore2022.csv', delimiter=';')
+df1 = pd.read_csv('Database/datasetFlashScore.csv', delimiter=';')
+df2 = pd.read_csv('Database/datasetFlashScore2022.csv', delimiter=';')
 
 # Concatena os dois dataframes
-df = pd.concat([df1, df2])
+df = pd.concat([df1, df2], ignore_index = True)
 
 # Cria um atributo para o resultado do jogo, servindo como target da IA
 df['Resultado'] = df.apply(lambda row: "Vitória do time da casa" if int(row['FTHG']) > int(row['FTAG']) else "Vitória do time visitante" if int(row['FTHG']) < int(row['FTAG']) else "Empate", axis=1)
 
 # Cria uma coluna para indicar se o jogo teve 3 gols ou mais
 df['Over 2.5'] = df.apply(lambda row: 1 if int(row['FTHG']) + int(row['FTAG']) >= 3 else 0, axis=1)
+df = df.drop(columns=['FTAG', 'FTHG'])
+dfTestes = df.head(20)
+dfTreinamento = df.iloc[20:]
 
-print(df)
+from pycaret.classification import *
+s = setup(dfTreinamento, target = 'Over 2.5', session_id = 123)
+
+best = compare_models()
+
+tuned = tune_model(best)
+
+predictions = predict_model(tuned, data=dfTestes, raw_score = True)
+
+
+
--- main.py
+++ main.py
@@ -1,20 +1,10 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
-db = SQLAlchemy(app)
 
 from routes import *
-from Cache.FootballdataukDataScience.models import *
-from Cache.FootballdataukDataScience.datascience import atualizarDB, corrigirCSVs
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.drop_all()
-        db.create_all()
-        corrigirCSVs()
-        atualizarDB()
-    
     app.run(debug=True, host = '0.0.0.0')
 
